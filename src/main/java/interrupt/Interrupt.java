7c7f743bc48a726d0c2a3eaf9cfb76b739ec064f
Alexandra Cail <acail@acail-a01.vmware.com>
2021-03-03 16:10:35 -0500

    Updated Interrupt.java

--- src/main/java/interrupt/Interrupt.java
+++ src/main/java/interrupt/Interrupt.java
@@ -7,9 +7,30 @@ import com.slack.api.bolt.jetty.SlackAppServer;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.slack.api.model.event.AppMentionEvent;
 
+/**
+ * Listens for app mention in Slack channels and pings appropriate manager
+ * interrupt if within valid time window. Resources: Spring (https://spring.io/guides/gs/maven/#scratch),
+ * Java/Bolt Slack API (https://api.slack.com/start/building/bolt-java).
+ * @author Yugam Sharma
+ * @author Alex Cail
+ */
 public class Interrupt {
+
+  /** Instance of ScheduleUtil class. Assists with time/date validation and retrieving manager IDs. */
+  private static ScheduleUtil scheduleUtil;
+
+  /**
+   * Listens for app mention in Slack channels and calls upon scheduleUtil to
+   * validate time/date and return the ID of the manager interrupt on-duty. Given
+   * there is a manager, pings said manager. Otherwise, returns message stating that
+   * no manager interrupt is currently available.
+   * @param args Commmand line arguments.
+   * @throws Exception if any issues arise.
+   */
   public static void main(String[] args) throws Exception {
-     // Set app config
+    // Initalize ScheduleUtil object
+    scheduleUtil = new ScheduleUtil();
+    // Set app config
     var config = new AppConfig();
     config.setSingleTeamBotToken(System.getenv("SLACK_TOKEN"));
     config.setSigningSecret(System.getenv("SLACK_SIGNING_SECRET"));
@@ -22,13 +43,21 @@ public class Interrupt {
       System.out.println("ThreadTs: " + event.getThreadTs());
       System.out.println("Ts: " + event.getTs());
       System.out.println(event.getThreadTs() != null);
+      String manager = scheduleUtil.getManagerName(); // Validates date and time, parses Google sheet
+      String managerMessage;
+      if (manager == null) {
+        managerMessage = "No available manager interrupt on-duty at this time. Interrupt duty is within the hours of 12p-5p ET / 9a-2p PT on regular business days.";
+      } else {
+        managerMessage = "<@" + scheduleUtil.getManagerID(manager) + ">"; // Parses env var
+      }
       if(event.getThreadTs() != null){
         if(event.getThreadTs() != event.getTs()) {
           ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
           .token(ctx.getBotToken())
           .channel(event.getChannel())
           .threadTs(event.getThreadTs())
-          .text("<@" + event.getUser() + "> Thank you! We greatly appreciate your efforts :two_hearts:"));
+          //.text("<@" + event.getUser() + "> Thank you! We greatly appreciate your efforts :two_hearts:"));
+          .text(managerMessage));
           if (!message.isOk()) {
             ctx.logger.error("chat.postMessage failed: {}", message.getError());
           }
@@ -38,7 +67,8 @@ public class Interrupt {
           .token(ctx.getBotToken())
           .channel(event.getChannel())
           .threadTs(event.getTs())
-          .text("<@" + event.getUser() + "> Thank you! We greatly appreciate your efforts :raised_hands:"));
+          //.text("<@" + event.getUser() + "> Thank you! We greatly appreciate your efforts :raised_hands:"));
+          .text(managerMessage));
           if (!message.isOk()) {
             ctx.logger.error("chat.postMessage failed: {}", message.getError());
           }
